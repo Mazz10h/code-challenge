c438bf09903a8c77beed61cc2eb1f0d7c1556d84
Fainus Gabriel <nusgabriel17@gmail.com>
2025-09-09 17:23:43 +0300

    changes

--- grade.js
+++ grade.js
@@ -1,17 +1,26 @@
-const marks = 45;
-
+const marks = 40;
+// Check if marks are between 80 and 100 — Grade A
 if (marks >= 79 && marks <= 100) {
   console.log("A");
+  // Check if marks are between 60 and 78 — Grade B
 } else if (marks >= 60 && marks < 79) {
   console.log("B");
-}
-else if (marks >= 49 && marks <= 59) {
+  // Check if marks are between 49 and 59 — Grade C
+
+} else if (marks >= 49 && marks <= 59) {
   console.log("C");
-}
-else if (marks > 40 && marks <= 49) {
+  // Check if marks are between 41 and 48 — Grade D
+} else if (marks >= 40 && marks <= 49) {
   console.log("D");
+
+// Check if marks are below 40 — Grade E
 } else if (marks >= 0 && marks < 40) {
   console.log("E");
+  // If the marks are outside the range of 0–100, it's invalid
 } else {
   console.log("Invalid marks");
 }
+/**
+ * This code is for grading system.
+ * 
+ */
